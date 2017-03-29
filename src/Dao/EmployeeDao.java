e84bf43fcdc9a2cad81f156bddce0565a03dcc7a
Tarun <tarun.bargadss@gmail.com>
2017-03-29 19:19:38 +0530

    hibernate changes

---
+++ src/Dao/EmployeeDao.java
@@ -0,0 +1,70 @@
+package Dao;
+
+
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import Bean.Employee;
+ 
+
+
+public class EmployeeDao {
+   private static SessionFactory factory; 
+   public static void main(String[] args) {
+      try{
+         factory = new Configuration().configure().buildSessionFactory();
+      }catch (Throwable ex) { 
+         System.err.println("Failed to create sessionFactory object." + ex);
+         throw new ExceptionInInitializerError(ex); 
+      }
+      EmployeeDao ME = new EmployeeDao();
+
+     
+      Integer empID1 = ME.addEmployee("asd", "xcv", 1000);
+      Integer empID2 = ME.addEmployee("ert", "qsw", 5000);
+      Integer empID3 = ME.addEmployee("fgh", "jko", 10000);
+
+      ME.updateEmployee(empID1, 5000);
+      }
+   
+   public Integer addEmployee(String fname, String lname, int salary){
+      Session session = factory.openSession();
+      Transaction tx = null;
+      Integer employeeID = null;
+      try{
+         tx = session.beginTransaction();
+         Employee employee = new Employee(fname, lname, salary);
+         employeeID = (Integer) session.save(employee); 
+         tx.commit();
+      }catch (HibernateException e) {
+         if (tx!=null) tx.rollback();
+         e.printStackTrace(); 
+      }finally {
+         session.close(); 
+      }
+      return employeeID;
+   }
+   
+  
+   public void updateEmployee(Integer EmployeeID, int salary ){
+      Session session = factory.openSession();
+      Transaction tx = null;
+      try{
+         tx = session.beginTransaction();
+         Employee employee = 
+                    (Employee)session.get(Employee.class, EmployeeID); 
+         employee.setSalary( salary );
+		 session.update(employee); 
+         tx.commit();
+      }catch (HibernateException e) {
+         if (tx!=null) tx.rollback();
+         e.printStackTrace(); 
+      }finally {
+         session.close(); 
+      }
+   }
+    }
